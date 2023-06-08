5538dcb1fba9ab0f2804dd2ae3eb2df6e80de2e9
DWisniewski79 <45345360+DWisniewski79@users.noreply.github.com>
2023-06-08 10:51:42 +1000

    Added 2d assets

---
+++ Source/froggerClone/defaultCamera.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraActor.h"
+#include "defaultCamera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FROGGERCLONE_API AdefaultCamera : public ACameraActor
+{
+	GENERATED_BODY()
+	
+};
