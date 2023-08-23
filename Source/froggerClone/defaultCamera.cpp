b155ac30f7d50e89f1dbe55e34414fd42e80f04e
DWisniewski79 <45345360+DWisniewski79@users.noreply.github.com>
2023-08-23 22:31:21 +1000

    Working on Camera Bounds
    Adding functionality for camera bounds. Uncertain if this will be the final approach but it's a start

--- Source/froggerClone/defaultCamera.cpp
+++ Source/froggerClone/defaultCamera.cpp
@@ -10,6 +10,30 @@
 
 void AdefaultCamera::BeginPlay()
 {
+	FVector CamLoc;
+	FVector CamLocTwo;
+	FRotator CamRot;
+	FRotator CamRotTwo;
+	int x;
+	int y;
 	APlayerController* OurPlayerController = UGameplayStatics::GetPlayerController(this, 0);
 	OurPlayerController->SetViewTarget(myCamera);
+	OurPlayerController->GetPlayerViewPoint(CamLoc, CamRot);
+	OurPlayerController->GetViewportSize(x, y);
+	FVector CameraBounds;
+	CameraBounds.Set((float)x, (float)y, CamLoc.Z);
+	myCamera->GetActorEyesViewPoint(CamLocTwo, CamRotTwo);
+
+	FVector ViewPos = OurPlayerController->GetFocalLocation();
+
+	ViewPos.X = FMath::Clamp(ViewPos.X, CameraBounds.X, CameraBounds.X * -1);
+	ViewPos.Y = FMath::Clamp(ViewPos.Y, CameraBounds.Y, CameraBounds.Y * -1);
+	//OurPlayerController->SetActorLocation(ViewPos);
+
+	UE_LOG(LogTemp, Warning, TEXT("x value: %d, y value: %d"), x,y);
+	UE_LOG(LogTemp, Warning, TEXT("cameraLoc2: %s"), *CamLocTwo.ToString());
+	UE_LOG(LogTemp, Warning, TEXT("camera bounds: %s"), *CameraBounds.ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Not sure what this will print: %s"), *ViewPos.ToString());
+
+	
 }
